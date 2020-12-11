460075065cc74a72e9ea878277585ed5788e2225
SaurabhT311 <tripathisaurabh.311@gmail.com>
2020-12-12 01:06:47 +0530

    Displaying the result of flipcoin

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,1 +1,16 @@
-echo "welcome"
+#!/usr/bin/bash -x
+
+echo "Well To Flip Coin Simulator"
+
+read -p "Do you want to flip a coin (y/n) : " permission
+
+if [ $permission == "y" ]
+then
+	flipResult=$(( RANDOM%2 ))
+	if [ $flipResult -eq 0 ]
+	then
+		echo "Heads"
+	else
+		echo "Tails"
+	fi
+fi
