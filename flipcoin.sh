f780cdd90b63df7a48b7ef2135b1096f501691c9
SaurabhT311 <tripathisaurabh.311@gmail.com>
2020-12-12 01:38:12 +0530

    Modified UC2 and continued till either one of win 21 times

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/bash -x
+noOfHeads=0
+noOfTails=0
 
-echo "Well To Flip Coin Simulator"
+echo "Welcome To Flip Coin Simulator"
 
-read -p "Do you want to flip a coin (y/n) : " permission
-
-if [ $permission == "y" ]
-then
+while [[ $noOfHeads -ne 21 && $noOfTails -ne 21 ]]
+do
 	flipResult=$(( RANDOM%2 ))
 	if [ $flipResult -eq 0 ]
 	then
 		echo "Heads"
+		noOfHeads=$(( $noOfHeads + 1 ))
 	else
 		echo "Tails"
+		noOfTails=$(( $noOfTails + 1 ))
 	fi
+done
+
+if [ $noOfHeads -eq 21 ]
+then
+	winMargin=$(( $noOfHeads - $noOfTails ))
+	echo "Heads won by $winMargin over Tails"
+else
+	winMargin=$(( $noOfTails - $noOfHeads ))
+	echo "Tails won by $winMargin over Heads"
 fi
